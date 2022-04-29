7b97039d43a968564b1af1901bc6c18acea2d481
CepotOi <rizkihutama69@gmail.com>
2022-04-30 01:13:31 +0700

    adding styling for ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,13 @@
+import './ExpenseItem.css';
+
 export default function ExpenseItem() {
   return (
-    <>
-      <h2>Expense Item</h2>
-    </>
+    <div className="expense-item">
+      <div>May 20th 2022</div>
+      <div className="expense-item__description">
+        <h2>GTR 34 V SPEC NUR</h2>
+        <div className="expense-item__price">$5000</div>
+      </div>
+    </div>
   );
 };
